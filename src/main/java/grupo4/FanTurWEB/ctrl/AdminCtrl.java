a1c8cb4573cadb4b3fef9ad5b69ec32c51a012ab
Gabriel Agustín VÉNTOLA <tuto_inxs@ca.frre.utn.edu.ar>
2018-07-01 13:07:23 -0300

    Controller padre con funcionalidad CRUD. A partir de acá, los demás controllers heredan y hacen solo lógica de negocio

---
+++ src/main/java/grupo4/FanTurWEB/ctrl/AdminCtrl.java
@@ -0,0 +1,37 @@
+package grupo4.FanTurWEB.ctrl;
+
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+import javax.ws.rs.core.Response;
+
+import grupo4.FanTurWEB.model.Admin;
+import grupo4.FanTurWEB.model.Ubicacion;
+import grupo4.FanTurWEB.model.dao.interfaces.AdminDao;
+
+@Named()
+@ViewScoped
+public class AdminCtrl extends Ctrl<Admin> implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@PostConstruct
+	private void init() {
+		modelObj =  new Admin();
+		client = ClientBuilder.newClient();
+		webTarget = client.target("http://localhost:8080/FanTurWEB/rest/admin");		
+	}	
+
+}
---
+++ src/main/java/grupo4/FanTurWEB/ctrl/Ctrl.java
@@ -0,0 +1,65 @@
+package grupo4.FanTurWEB.ctrl;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+public abstract class Ctrl<Model> {
+
+	protected Client client;
+	protected WebTarget webTarget;
+	protected Invocation invocation;
+	protected Response response;
+	protected Model modelObj;
+	protected String id;
+	
+	public Model getModelObj() {
+		return modelObj;
+	}
+	
+	public void setModelObj(Model modelObj) {
+		this.modelObj = modelObj;
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String get() {
+		invocation = webTarget.path(id).request().buildGet();
+		response = invocation.invoke();
+		return "este";
+	}
+	
+	public String getAll() {
+		invocation = webTarget.request().buildGet();
+		response = invocation.invoke();
+		return "todos";
+	}
+	
+	public String create() {
+		invocation = webTarget.request().buildPost(Entity.entity(modelObj, MediaType.APPLICATION_JSON));
+		response = invocation.invoke();
+		return "creado, papu";
+	}
+	
+	public String update() {
+		invocation = webTarget.request().buildPut(Entity.entity(modelObj, MediaType.APPLICATION_JSON));
+		response = invocation.invoke();
+		return "actualizado, papu";
+	}
+	
+	public String delete() {
+		invocation = webTarget.path(id).request().buildDelete();
+		response = invocation.invoke();
+		return "eliminado, papu";
+	}
+}
